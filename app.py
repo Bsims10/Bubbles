b28fa7950d7c7179fcd238cb067b6d6e55e0ef59
Brittany <ux.brittany@gmail.com>
2024-05-01 16:28:44 -0500

    python

---
+++ app.py
@@ -0,0 +1,11 @@
+character_name = "Bubbles"
+character_age = "58"
+is_female = True
+print("There once was a woman named " + character_name + ", ")
+print("She was " + character_age + " years old. ")
+print("She really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+character_name = "Sarah"
+print("She really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ", ")
